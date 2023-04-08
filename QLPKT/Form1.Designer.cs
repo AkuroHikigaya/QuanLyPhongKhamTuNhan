93cbf40650795835ede0ad3599c02e289096d73d
Akuro <luongluuminhtan@gmail.com>
2023-04-08 08:39:58 +0700

    Project 100%

--- QLPKT/Form1.Designer.cs
+++ QLPKT/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -88,23 +89,23 @@
             // 
             this.lb_hint_pw.AutoSize = true;
             this.lb_hint_pw.Enabled = false;
-            this.lb_hint_pw.Font = new System.Drawing.Font("UTM BryantLG", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_hint_pw.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_hint_pw.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.lb_hint_pw.Location = new System.Drawing.Point(183, 282);
             this.lb_hint_pw.Name = "lb_hint_pw";
-            this.lb_hint_pw.Size = new System.Drawing.Size(142, 24);
+            this.lb_hint_pw.Size = new System.Drawing.Size(137, 24);
             this.lb_hint_pw.TabIndex = 20;
             this.lb_hint_pw.Text = "Nhập mật khẩu";
             // 
             // tb_password
             // 
             this.tb_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tb_password.Font = new System.Drawing.Font("UTM BryantLG", 15F);
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             this.tb_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.tb_password.Location = new System.Drawing.Point(178, 282);
             this.tb_password.Name = "tb_password";
             this.tb_password.PasswordChar = '*';
-            this.tb_password.Size = new System.Drawing.Size(277, 25);
+            this.tb_password.Size = new System.Drawing.Size(277, 23);
             this.tb_password.TabIndex = 2;
             this.tb_password.TextChanged += new System.EventHandler(this.tb_password_TextChanged);
             this.tb_password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_password_KeyDown_1);
@@ -113,11 +114,11 @@
             // 
             this.lb_hint_us.AutoSize = true;
             this.lb_hint_us.Enabled = false;
-            this.lb_hint_us.Font = new System.Drawing.Font("UTM BryantLG", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_hint_us.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_hint_us.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.lb_hint_us.Location = new System.Drawing.Point(183, 217);
             this.lb_hint_us.Name = "lb_hint_us";
-            this.lb_hint_us.Size = new System.Drawing.Size(173, 24);
+            this.lb_hint_us.Size = new System.Drawing.Size(167, 24);
             this.lb_hint_us.TabIndex = 19;
             this.lb_hint_us.Text = "Nhập tên tài khoản";
             // 
@@ -129,7 +130,7 @@
             this.btn_login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_login.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_login.Font = new System.Drawing.Font("UTM BryantLG", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.btn_login.ForeColor = System.Drawing.Color.White;
             this.btn_login.Location = new System.Drawing.Point(198, 356);
             this.btn_login.Name = "btn_login";
@@ -150,11 +151,11 @@
             // tb_username
             // 
             this.tb_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tb_username.Font = new System.Drawing.Font("UTM BryantLG", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.tb_username.Location = new System.Drawing.Point(178, 217);
             this.tb_username.Name = "tb_username";
-            this.tb_username.Size = new System.Drawing.Size(277, 25);
+            this.tb_username.Size = new System.Drawing.Size(277, 23);
             this.tb_username.TabIndex = 1;
             this.tb_username.TextChanged += new System.EventHandler(this.tb_username_TextChanged);
             this.tb_username.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_username_KeyDown);
@@ -219,11 +220,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Font = new System.Drawing.Font("UTM BryantLG", 21.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.25F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.LimeGreen;
-            this.label2.Location = new System.Drawing.Point(46, 109);
+            this.label2.Location = new System.Drawing.Point(29, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(521, 36);
+            this.label2.Size = new System.Drawing.Size(539, 33);
             this.label2.TabIndex = 14;
             this.label2.Text = "PHẦN MỀM QUẢN LÝ PHÒNG KHÁM";
             // 
@@ -231,13 +232,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("UTM BryantLG", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(407, 79);
+            this.label1.Location = new System.Drawing.Point(405, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 30);
+            this.label1.Size = new System.Drawing.Size(162, 29);
             this.label1.TabIndex = 15;
             this.label1.Text = "ĐĂNG NHẬP";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
             // 
             // Form1
             // 
@@ -281,6 +286,7 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.TextBox tb_password;
         private System.Windows.Forms.Label lb_hint_pw;
+        private System.Windows.Forms.PrintDialog printDialog1;
     }
 }
 
